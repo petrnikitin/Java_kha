71d2c207541e8316dc2907aa07d91387de465816
Peter <harmonyjkee@gmail.com>
2018-10-24 23:32:05 +0300

    home Work

--- src/BMIHomeWork.java
+++ src/BMIHomeWork.java
@@ -1,11 +1,11 @@
 public class BMIHomeWork {
     public static void main(String[] args) {
-        double bmi = bmimethod(90, 200);
+        double bmi = bmimethod(90, 180);
         System.out.println(bmi);
     }
 
-    static float bmimethod(int weight, float height) {
-        height = (float) Math.pow(height/100, 2);
+    static float bmimethod(int weight, double height) {
+        height = Math.pow(height/100, 2);
         double res = weight/height;
         return (float) res;
     }
