698436e17c32315e50d054aa185031295cbc9b00
Peter <harmonyjkee@gmail.com>
2018-10-26 00:19:09 +0300

    home Work

--- src/Scanner/TestScannerdiff.java
+++ src/Scanner/TestScannerdiff.java
@@ -1,10 +1,14 @@
 package Scanner;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TestScannerdiff {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.getDefault());
+
+        Locale locale = Locale.getDefault();
+        System.out.println(locale.getCountry());
 
         System.out.println("Введите число");
         int i = scanner.nextInt();
